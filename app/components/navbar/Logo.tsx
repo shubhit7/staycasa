2d5d50c19eaf2b9c836778de8f7523643605b519
Shubhit Dhiman <shubhit.dhiman@gmail.com>
2023-06-10 18:08:31 +0530

    feat: UI for Navigation bar

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -1,21 +1,21 @@
-'use client';
+"use client";
 
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 const Logo = () => {
-  const router = useRouter();
+    const router = useRouter();
 
-  return ( 
-    <Image
-      onClick={() => router.push('/')}
-      className="hidden md:block cursor-pointer" 
-      src="/logo.svg" 
-      height="130" 
-      width="130" 
-      alt="staycasa" 
-    />
-   );
-}
- 
+    return (
+        <Image
+            onClick={() => router.push("/")}
+            className="hidden md:block cursor-pointer"
+            src="/logo.svg"
+            height="140"
+            width="140"
+            alt="staycasa"
+        />
+    );
+};
+
 export default Logo;
