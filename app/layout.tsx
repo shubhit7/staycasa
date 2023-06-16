5370c3d6623d1da162b3ae8e78ac7850fe73c224
Shubhit Dhiman <shubhit.dhiman@gmail.com>
2023-06-16 23:57:31 +0530

    feat(Button,Modal): Implement core components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,29 @@
-import Navbar from './components/navbar/Navbar'
-import './globals.css'
-import { Nunito } from 'next/font/google'
+import Modal from "./components/modals/Modal";
+import Navbar from "./components/navbar/Navbar";
+import "./globals.css";
+import { Nunito } from "next/font/google";
 
-const font = Nunito({ 
-  subsets: ['latin'] 
-})
+const font = Nunito({
+    subsets: ["latin"],
+});
 
 export const metadata = {
-  title: 'staycasa',
-  description: 'Home away from Home',
-}
+    title: "staycasa",
+    description: "Home away from Home",
+};
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={font.className}>
-        <Navbar />
-        {children}
-      </body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body className={font.className}>
+                <Modal actionLabel="Submit" title="Hello" isOpen/>
+                <Navbar />
+                {children}
+            </body>
+        </html>
+    );
 }
