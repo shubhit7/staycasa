933feb479a7a19b6e7506114785ce8eb88bc5e5c
Shubhit Dhiman <shubhit.dhiman@gmail.com>
2023-07-10 00:21:04 +0530

    feat: Implement rent modal hook

---
+++ app/components/modals/RentModal.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Modal from "./Modal";
+
+const RentModal = () => {
+    return (
+        <Modal
+            title="List your home!"
+        />
+    );
+};
+
+export default RentModal;
--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -7,6 +7,7 @@ import MenuItem from "./MenuItem";
 
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 import useLoginModal from "@/app/hooks/useLoginModal";
+import useRentModal from "@/app/hooks/useRentModal";
 import { signOut } from "next-auth/react";
 import { SafeUser } from "@/app/types";
 
@@ -18,15 +19,24 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
     const [isOpen, setIsOpen] = useState(false);
     const registerModal = useRegisterModal();
     const loginModal = useLoginModal();
+    const rentModal = useRentModal();
 
     const toggleOpen = useCallback(() => {
         setIsOpen((value) => !value);
     }, []);
+
+    const onRent = useCallback(() => {
+        if (!currentUser) {
+            return loginModal.onOpen();
+        }
+        rentModal.onOpen();
+    }, [loginModal, rentModal, currentUser]);
+
     return (
         <div className="relative">
             <div className="flex flex-row items-center gap-3">
                 <div
-                    onClick={() => {}}
+                    onClick={onRent}
                     className="
                         hidden
                         md:block
